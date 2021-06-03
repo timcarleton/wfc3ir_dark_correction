26fc914c59404ee021d6598f8c9274cc3e5face0
Timothy Carleton <tcn@cluster.sese.asu.edu>
2021-06-03 11:16:51 -0700

    recognizes when no pflt or drk

--- deltadarkcorr.py
+++ deltadarkcorr.py
@@ -8,8 +8,21 @@ def getdarkcorrimg(original,refdir='/store/skysurf/',rawloc='find'):
 
     flt=fits.open(original)
 
-    fdrk=fits.open(refdir+'crds_cache/references/hst/wfc3/'+flt[0].header['DARKFILE'].split('$')[-1])
-    fflt=fits.open(refdir+'crds_cache/references/hst/wfc3/'+flt[0].header['PFLTFILE'].split('$')[-1])
+    if 'N/A' not in flt[0].header['DARKFILE']:
+        fdrk=fits.open(refdir+'crds_cache/references/hst/wfc3/'+flt[0].header['DARKFILE'].split('$')[-1])
+        fdrkdata=fdrk['SCI'].data
+    else:
+        print('no dark file, correction not performed')
+        return -1
+
+        
+    if 'N/A' not in flt[0].header['PFLTFILE']:
+        fflt=fits.open(refdir+'crds_cache/references/hst/wfc3/'+flt[0].header['PFLTFILE'].split('$')[-1])
+        ffltdata=fflt['SCI'].data
+    else:
+        ffltdata=np.ones_like(flt['SCI'].data)
+
+        
     if 'N/A' not in flt[0].header['DFLTFILE']:
         dflt=fits.open(refdir+'crds_cache/references/hst/wfc3/'+flt[0].header['DFLTFILE'].split('$')[-1])
         dfltdata=dflt['SCI'].data
@@ -29,12 +42,21 @@ def getdarkcorrimg(original,refdir='/store/skysurf/',rawloc='find'):
     moddark=darkmodel2.darkmodelfile(original,rwloc+root+'_raw.fits')
 
     if flt['SCI'].data.shape[0]<1014:
-        offsety,offsetx=-int(flt[1].header['LTV1'])+5,-int(flt[1].header['LTV2'])+5
-        ffltdat=fflt['SCI'].data[offsetx:-offsetx,offsety:-offsety]
-        dfltdat=dfltdata[offsetx:-offsetx,offsety:-offsety]
+        if 'N/A' not in flt[0].header['PFLTFILE']:
+            offsety,offsetx=-int(flt[1].header['LTV1'])+5,-int(flt[1].header['LTV2'])+5
+            print(ffltdata.shape)
+            ffltdat=ffltdata[offsetx:-offsetx,offsety:-offsety]
+            dfltdat=dfltdata[offsetx:-offsetx,offsety:-offsety]
+        else:
+            ffltdat=ffltdata
+            dfltdat=dfltdata
     else:
-        ffltdat=fflt['SCI'].data[5:-5,5:-5]
-        dfltdat=dfltdata[5:-5,5:-5]
+        if 'N/A' not in flt[0].header['PFLTFILE']:
+            ffltdat=ffltdata[5:-5,5:-5]
+            dfltdat=dfltdata[5:-5,5:-5]
+        else:
+            ffltdat=ffltdata
+            dfltdat=dfltdata
         
     drkdat=fdrk['SCI'].data[5:-5,5:-5]
 
